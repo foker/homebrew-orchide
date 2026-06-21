c241144a180cfd94be699dbbcf1472fb58e96f98
Gerasimov Nikita <gerasimow.nikita@gmail.com>
2026-06-22 01:43:32 +0700

    orchide 0.2.1

--- Casks/orchide.rb
+++ Casks/orchide.rb
@@ -1,6 +1,6 @@
 cask "orchide" do
-  version "0.2.0"
-  sha256 "52729db8f639446526001d22b325332dddec05f4303225281d66bfc5d42aa452"
+  version "0.2.1"
+  sha256 "1df8c1fe960cd760eeac1b29293b4b3d823279c21852b29148d323cf96b767c1"
 
   url "https://github.com/foker/orch-ide/releases/download/v#{version}/OrchIDE-#{version}-macos-arm64.tar.gz"
   name "OrchIDE"
